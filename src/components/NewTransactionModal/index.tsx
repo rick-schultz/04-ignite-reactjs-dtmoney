75c2edc76733f57c7d76adc776bb370f81f6d8cc
Rick Schultz <rick-schultz@hotmail.com>
2021-10-01 11:23:53 -0300

    install polished and create modal style

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,8 @@
 import Modal from 'react-modal';
+import { Container, TransactionTypeContainer } from './styles';
+import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +14,48 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
     <Modal
     isOpen={isOpen}
     onRequestClose={onRequestClose}
+    overlayClassName="react-modal-overlay"
+    className="react-modal-content"
     >
-     <h2>Register Transaction</h2>
+     <button
+     type="button"
+     onClick={onRequestClose}
+     className="react-modal-close"
+     ><img src={closeImg} alt="close modal" /></button>
+     <Container>
+       <h2>New Transaction</h2>
+       <input
+       type="text"
+       placeholder="Title"
+       />
+
+       <input
+       type="number"
+       placeholder="Amount"
+       />
+
+      <TransactionTypeContainer>
+        <button
+        type="button"
+        >
+          <img src={incomeImg} alt="income" />
+          <span>Income</span>
+        </button>
+        <button
+        type="button"
+        >
+          <img src={outcomeImg} alt="outcome" />
+          <span>Outcome</span>
+        </button>
+      </TransactionTypeContainer>
+
+       <input
+       type="text"
+       placeholder="Category"
+       />
+
+       <button type="submit">Register</button>
+     </Container>
   </Modal>
   )
 }
