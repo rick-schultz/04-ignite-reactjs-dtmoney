40637bd1c1c2176be2de95e85fc0049296eb44f0
Rick Schultz <rick-schultz@hotmail.com>
2021-10-01 10:42:44 -0300

    move modal to new component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root');
@@ -20,12 +21,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
-      <Modal
-        isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}
-        >
-         <h2>Register Transaction</h2>
-      </Modal>
+      <NewTransactionModal
+      isOpen={isNewTransactionModalOpen}
+      onRequestClose={handleCloseNewTransactionModal}/
+      >
       <GlobalStyle/>
     </>
   );
